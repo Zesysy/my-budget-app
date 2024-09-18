de8b2116473bb930e486fd4e21d2607cb26bb40d
Zesysy <sylene.manusset@gmail.com>
2024-09-18 12:00:51 +0200

    [WIP] mise en place de prettier et eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],  theme: {
+module.exports =  {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  important: '#root',
+  theme: {
     extend: {},
   },
+  corePlugins: {
+    // Remove the Tailwind CSS preflight styles so it can use Material UI's preflight instead (CssBaseline).
+    preflight: false,
+  },
   plugins: [],
-}
+};
 
